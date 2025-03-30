a642117dbc9a7b956eba78f9106b92def87cba7b
nedalMohamed <nedal mohamed201@gmail.com>
2025-03-30 08:39:24 -0700

    Initial commit

---
+++ javascript/details.js
@@ -0,0 +1,41 @@
+
+
+function loadProductDetails() {
+    const product = JSON.parse(localStorage.getItem("selectedProduct"));
+    if (product) {
+        document.getElementById("product-name").innerText = product.name;
+        document.getElementById("product-description").innerText = product.description;
+        document.getElementById("product-price").innerText = "Price: " + product.price + " EGP";
+        document.getElementById("product-image").src = product.image;
+        document.getElementById("product-des").innerText = product.dest;
+        document.getElementById("product-specifications").innerText = product.specifications;
+
+        loadRelatedProducts(product.category);
+    } else {
+        document.querySelector(".product-details").innerHTML = "<p>لم يتم العثور على المنتج</p>";
+    }
+}
+
+// function loadRelatedProducts(category) {
+//     let relatedContainer = document.getElementById("related-products");
+//     relatedContainer.innerHTML = "";
+
+//     let related = products.filter(p => p.category === category).slice(0, 3);
+//     related.forEach(product => {
+//         let div = document.createElement("div");
+//         div.classList.add("related-product");
+//         div.innerHTML = `
+//             <img src="${product.image}" alt="${product.name}">
+//             <h3>${product.name}</h3>
+//             <p>${product.price} جنيه</p>
+//         `;
+//         div.onclick = () => {
+//             localStorage.setItem("selectedProduct", JSON.stringify(product));
+//             window.location.reload();
+//         };
+//         relatedContainer.appendChild(div);
+//     });
+// }
+
+window.onload = loadProductDetails;
+{/* </script> */}
---
+++ javascript/nav2.js
@@ -0,0 +1,18 @@
+function loadProductDetails() {
+    const product = JSON.parse(localStorage.getItem("selectedProduct"));
+    if (product) {
+        document.getElementById("product-name").innerText = product.name;
+        document.getElementById("product-description").innerText = product.description;
+        // document.getElementById("product-price").innerText = "Price: " + product.price + " EGP";
+        document.getElementById("product-image").src = product.image;
+        document.getElementById("product-des").innerText = product.dest;
+        document.getElementById("product-specifications").innerText = product.specifications;
+
+        loadRelatedProducts(product.category);
+    } else {
+        document.querySelector(".product-details").innerHTML = "<p>لم يتم العثور على المنتج</p>";
+    }
+}
+
+window.onload = loadProductDetails;
+{/* </script> */}
